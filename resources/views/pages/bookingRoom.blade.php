5b6ce5e1ea97d28f29b077811d7bfc18d24914fc
DennyRengganis <drdennyr1245@gmail.com>
2019-08-01 14:44:47 +0700

    layout booking page

--- resources/views/pages/bookingRoom.blade.php
+++ resources/views/pages/bookingRoom.blade.php
@@ -2,36 +2,96 @@
 @section('css')
 <link rel="stylesheet" type="text/css" href="/css/open-iconic-bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/css/size.css">
+<link rel="stylesheet" type="text/css" href="/css/page.css">
 @endsection
 @section('content')
+<div class="header">
+  <h1>Booking form</h1>
+</div>
 <form action="/action_page.php">
-  Building :
-  <select name="Building">
-    <option value="ACCF">ACCF</option>
-    <option value="TBS">TBS</option>
-  </select>
-  Floor: 
-    <select name="Floor">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="4">5</option>
-  </select>
-  Star Booking :
-  <input type="date" name="start_booking">
-  End Booking :
-  <input type="date" name="end_booking">
-  Purpose :
-  <input type="text" name="purpose" value="purpose">
-  NPK :
-  <input type="integer" name="npk" value="npk">
-  Nama :
-  <input type="text" name="nama" value="nama">
-  Email:
-  <input type="text" name="email" value="email">
-  <br><br>
-  <input type="submit" value="Book">
+  <div class="row">
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+    <div class="col-w-1 col-s-1">h</div>
+  </div>
+  <div class="row">
+    <div class="col-w-1"></div>
+    <div class="col-w-4 center">
+      Building :
+      <select name="Building">
+        <option value="ACCF">ACCF</option>
+        <option value="TBS">TBS</option>
+      </select>
+    </div>
+    <div class="col-w-1"></div>
+    <div class="col-w-2 center">
+      Floor: 
+      <select name="Floor">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="4">5</option>
+      </select>
+    </div>
+    <div class="col-w-1"></div>
+    <div class="col-w-2 center">
+     Floor: 
+     <select name="Room">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="4">5</option>
+    </select>
+  </div>
+  <div class="col-w-1"></div>
+</div>
+<div class="row">
+  <div class="col-w-1"></div>
+  <div class="col-w-3 center">
+    Start Booking :
+    <input type="date" name="start_booking">
+  </div>
+  <div class="col-w-1"></div>
+  <div class="col-w-3 center">
+    End Booking :
+    <input type="date" name="end_booking">
+  </div>
+  <div class="col-w-1"></div>
+  <div class="col-w-3 center">
+    Purpose :
+    <input type="text" name="purpose" value="purpose">
+  </div>
+</div>
+<div class="row">
+<div class="col-w-1"></div>
+<div class="col-w-3 center">
+NPK :
+<input type="integer" name="npk" value="npk">
+</div>
+<div class="col-w-1"></div>
+<div class="col-w-3 center">
+Nama :
+<input type="text" name="nama" value="nama">
+</div>
+<div class="col-w-1"></div>
+<div class="col-w-3 center">
+Email:
+<input type="text" name="email" value="email">
+</div>
+</div>
+<br><br>
+<input type="submit" value="Book">
 
 <script type="text/javascript" href="/js/bootstrap.bundle.js" ></script>
 @endsection
--- resources/views/pages/homePage.blade.php
+++ resources/views/pages/homePage.blade.php
@@ -2,7 +2,7 @@
 @section('css')
 <link rel="stylesheet" type="text/css" href="/css/open-iconic-bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/css/size.css">
-<link rel="stylesheet" type="text/css" href="/css/homePage.css">
+<link rel="stylesheet" type="text/css" href="/css/page.css">
 @endsection
 @section('content')
 <div class="header">
@@ -10,97 +10,97 @@
 </div>
 <form action="/action_page.php">
 	<div class="row">
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-	<div class="col-w-1 col-s-1">h</div>
-</div>
-<!-- <div class="row">
-	<div class="example">jkdfjdskjdf</div>
-</div> -->
-<div class="row">
-	<div class="col-w-1 col-s-1"></div>
-	<div class="col-w-2 col-s-2 center">
-		<div class="dropdownbox">
-			Building :
-			<select name="Building">
-				<option value="ACCF">ACCF</option>
-				<option value="TBS">TBS</option>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+		<div class="col-w-1 col-s-1">h</div>
+	</div>
+	<div class="row">
+		<div class="example">jkdfjdskjdf</div>
+	</div>
+	<div class="row">
+		<div class="col-w-1 col-s-1"></div>
+		<div class="col-w-2 col-s-2 center">
+			<div class="dropdownbox">
+				Building :
+				<select name="Building">
+					<option value="ACCF">ACCF</option>
+					<option value="TBS">TBS</option>
+				</select>
+			</div>
+		</div>
+		<div class="col-w-4 col-s-2"></div>
+		<div class="col-w-2 col-s-2 center">
+			Floor :
+			<select name="Floor">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="4">5</option>
 			</select>
 		</div>
-	</div>
-	<div class="col-w-4 col-s-2"></div>
-	<div class="col-w-2 col-s-2 center">
-		Floor :
-		<select name="Floor">
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-			<option value="4">4</option>
-			<option value="4">5</option>
-		</select>
-	</div>
-	<button type="submit" class="btn btn-primary col-w-1 col-s-2 center" value="search">search</button>
-</form>
-<button type="button" class="btn btn-primary col-w-1 col-s-2 center">+Booking</button>
-<div class="col-w-1 col-s-1"></div>
+		<button type="submit" class="btn btn-primary col-w-1 col-s-2 center" value="search">search</button>
+	</form>
+	<button type="button" class="btn btn-primary col-w-1 col-s-2 center">+Booking</button>
+	<div class="col-w-1 col-s-1"></div>
 </div>
 <div class="row">
-<div class="col-w-1 col-s-1"></div>
-<div class="col-w-10 col-s-10">
-	@foreach($liat as $room)
-	<tr>
-		<div class="col-w-4 col-s-4 center">
-			@if($room->status_now==="FREE")
-			<td>
-				{{$room->id}}
-				{{$room->nomor}}
-				{{$room->gedung}}
-				{{$room->lantai}}
-				{{$room->status_now}}
-				room is free
-				<span class="oi oi-plus"></span>
-				<br>
-			</td>
-			@endif
+	<div class="col-w-1 col-s-1"></div>
+	<div class="col-w-10 col-s-10">
+		@foreach($liat as $room)
+		<tr>
+			<div class="col-4 col-s-4 center">
+				@if($room->status_now==="FREE")
+				<td>
+					{{$room->id}}
+					{{$room->nomor}}
+					{{$room->gedung}}
+					{{$room->lantai}}
+					{{$room->status_now}}
+					room is free
+					<span class="oi oi-plus"></span>
+					<br>
+				</td>
+				@endif
 
-			@if($room->status_now==="BOOKED")
-			<td>
-				{{$room->id}}
-				{{$room->nomor}}
-				{{$room->gedung}}
-				{{$room->lantai}}
-				{{$room->status_now}}
-				room is booked
-				<button type="button" class="btn btn-primary">Schedule Meeting</button>
-				<br>
-			</td>
-			@endif
+				@if($room->status_now==="BOOKED")
+				<td>
+					{{$room->id}}
+					{{$room->nomor}}
+					{{$room->gedung}}
+					{{$room->lantai}}
+					{{$room->status_now}}
+					room is booked
+					<button type="button" class="btn btn-primary">Schedule Meeting</button>
+					<br>
+				</td>
+				@endif
 
-			@if($room->status_now==="WAITING")
-			<td>
-				{{$room->id}}
-				{{$room->nomor}}
-				{{$room->gedung}}
-				{{$room->lantai}}
-				{{$room->status_now}}
-				room is waiting
-				<button type="button" class="btn btn-primary">Schedule Meeting</button>
-				<br>
-			</td>
-			@endif
-		</div>
-	</tr>
-	@endforeach
-</div>
-<div class="col-w-1 col-s-1"></div>
+				@if($room->status_now==="WAITING")
+				<td>
+					{{$room->id}}
+					{{$room->nomor}}
+					{{$room->gedung}}
+					{{$room->lantai}}
+					{{$room->status_now}}
+					room is waiting
+					<button type="button" class="btn btn-primary">Schedule Meeting</button>
+					<br>
+				</td>
+				@endif
+			</div>
+		</tr>
+		@endforeach
+	</div>
+	<div class="col-1 col-s-1"></div>
 </div>
 @endsection
