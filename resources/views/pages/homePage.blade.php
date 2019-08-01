03c1995efee62e423aeafee6dfd7d03a23a18f49
DennyRengganis <drdennyr1245@gmail.com>
2019-08-01 10:43:41 +0700

    hehehe

--- resources/views/pages/homePage.blade.php
+++ resources/views/pages/homePage.blade.php
@@ -2,71 +2,85 @@
 @section('css')
 <link rel="stylesheet" type="text/css" href="/css/open-iconic-bootstrap.css">
 <link rel="stylesheet" type="text/css" href="/css/size.css">
+<link rel="stylesheet" type="text/css" href="/css/homePage.css">
 @endsection
 @section('content')
-<button type="button" class="btn btn-primary">+Booking</button>
+<div class="header">
+	<h1>Search Room</h1>
+</div>
 <form action="/action_page.php">
-	Building :
-  <select name="Building">
-    <option value="ACCF">ACCF</option>
-    <option value="TBS">TBS</option>
-  </select>
-  	Floor :
-    <select name="Floor">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="4">5</option>
-  </select>
-  <br><br>
-  <input type="submit" value="search">
+	<div class="col-1 col-s-1"></div>
+	<div class="col-2 col-s-2 center">
+		<div class="dropdownbox">
+			Building :
+			<select name="Building">
+				<option value="ACCF">ACCF</option>
+				<option value="TBS">TBS</option>
+			</select>
+		</div>
+	</div>
+	<div class="col-4 col-s-4"></div>
+	<div class="col-2 col-s-2 center">
+		Floor :
+		<select name="Floor">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="4">5</option>
+		</select>
+	</div>
+	<button type="submit" class="btn btn-primary col-1 center" value="search">search</button>
 </form>
-@foreach($liat as $room)
-<tr>
-	@if($room->status_now==="FREE")
-	<td>
-	{{$room->id}}
-	{{$room->nomor}}
-	{{$room->gedung}}
-	{{$room->lantai}}
-	{{$room->status_now}}
-	room is free
-	<span class="oi oi-plus"></span>
-	<br>
-	</td>
-	</div>
-	@endif
+<button type="button" class="btn btn-primary col-1 center">+Booking</button>
+<div class="col-1 col-s-1"></div>
+<div class="col-1 col-s-1"></div>
+<div class="col-1 col-s-1"></div>
+<div class="col-10 col-s-10">
+	@foreach($liat as $room)
+	<tr>
+		<div class="col-4 col-s-4 center">
+			@if($room->status_now==="FREE")
+			<td>
+				{{$room->id}}
+				{{$room->nomor}}
+				{{$room->gedung}}
+				{{$room->lantai}}
+				{{$room->status_now}}
+				room is free
+				<span class="oi oi-plus"></span>
+				<br>
+			</td>
+			@endif
 
-	@if($room->status_now==="BOOKED")
-	<td>
-	{{$room->id}}
-	{{$room->nomor}}
-	{{$room->gedung}}
-	{{$room->lantai}}
-	{{$room->status_now}}
-	room is booked
-	<button type="button" class="btn btn-primary">Schedule Meeting</button>
-	<br>
-	</td>
-	</div>
-	@endif
+			@if($room->status_now==="BOOKED")
+			<td>
+				{{$room->id}}
+				{{$room->nomor}}
+				{{$room->gedung}}
+				{{$room->lantai}}
+				{{$room->status_now}}
+				room is booked
+				<button type="button" class="btn btn-primary">Schedule Meeting</button>
+				<br>
+			</td>
+			@endif
 
-	@if($room->status_now==="WAITING")
-	<td>
-	{{$room->id}}
-	{{$room->nomor}}
-	{{$room->gedung}}
-	{{$room->lantai}}
-	{{$room->status_now}}
-	room is waiting
-	<button type="button" class="btn btn-primary">Schedule Meeting</button>
-	<br>
-	</td>
-	</div>
-
-
-	@endif
-</tr>
-@endforeach
+			@if($room->status_now==="WAITING")
+			<td>
+				{{$room->id}}
+				{{$room->nomor}}
+				{{$room->gedung}}
+				{{$room->lantai}}
+				{{$room->status_now}}
+				room is waiting
+				<button type="button" class="btn btn-primary">Schedule Meeting</button>
+				<br>
+			</td>
+			@endif
+		</div>
+	</tr>
+	@endforeach
+</div>
+<div class="col-1 col-s-1"></div>
 @endsection
