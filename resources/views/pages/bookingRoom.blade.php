7ffe01eb613ad7b1ba51e165724c7c5ecb11b61c
DennyRengganis <drdennyr1245@gmail.com>
2019-08-01 15:26:21 +0700

    layout booking room

--- resources/views/pages/bookingRoom.blade.php
+++ resources/views/pages/bookingRoom.blade.php
@@ -24,16 +24,16 @@
     <div class="col-w-1 col-s-1">h</div>
   </div>
   <div class="row">
-    <div class="col-w-1"></div>
-    <div class="col-w-4 center">
+    <div class="col-w-1 col-s-1"></div>
+    <div class="col-w-4 col-s-4 center">
       Building :
       <select name="Building">
         <option value="ACCF">ACCF</option>
         <option value="TBS">TBS</option>
       </select>
     </div>
-    <div class="col-w-1"></div>
-    <div class="col-w-2 center">
+    <div class="col-w-1 col-s-1"></div>
+    <div class="col-w-2 col-s-2 center">
       Floor: 
       <select name="Floor">
         <option value="1">1</option>
@@ -43,8 +43,8 @@
         <option value="4">5</option>
       </select>
     </div>
-    <div class="col-w-1"></div>
-    <div class="col-w-2 center">
+    <div class="col-w-1 col-s-1"></div>
+    <div class="col-w-2 col-s-2 center">
      Floor: 
      <select name="Room">
       <option value="1">1</option>
@@ -54,44 +54,48 @@
       <option value="4">5</option>
     </select>
   </div>
-  <div class="col-w-1"></div>
+  <div class="col-w-1 col-s-1"></div>
 </div>
 <div class="row">
-  <div class="col-w-1"></div>
-  <div class="col-w-3 center">
+  <div class="col-w-1 col-s-1"></div>
+  <div class="col-w-3 col-s-3 center">
     Start Booking :
     <input type="date" name="start_booking">
   </div>
-  <div class="col-w-1"></div>
-  <div class="col-w-3 center">
+  <div class="col-w-1 col-s-1"></div>
+  <div class="col-w-3 col-s-3 center">
     End Booking :
     <input type="date" name="end_booking">
   </div>
-  <div class="col-w-1"></div>
-  <div class="col-w-3 center">
+  <div class="col-w-3 col-s-3 center">
     Purpose :
     <input type="text" name="purpose" value="purpose">
   </div>
+  <div class="col-w-1 col-s-1"></div>
 </div>
 <div class="row">
-<div class="col-w-1"></div>
-<div class="col-w-3 center">
-NPK :
-<input type="integer" name="npk" value="npk">
+  <div class="col-w-1 col-s-1"></div>
+  <div class="col-w-3 col-s-3 center">
+    NPK :
+    <input type="integer" name="npk" value="npk">
+  </div>
+  <div class="col-w-1 col-s-1"></div>
+  <div class="col-w-3 col-s-3 center">
+    Nama :
+    <input type="text" name="nama" value="nama">
+  </div>
+  <div class="col-w-3 col-s-3 center">
+    Email:
+    <input type="text" name="email" value="email">
+  </div>
+  <div class="col-w-1 col-s-1"></div>
 </div>
-<div class="col-w-1"></div>
-<div class="col-w-3 center">
-Nama :
-<input type="text" name="nama" value="nama">
+<div class="row">
+  <div class="col-w-6 col-s-6"></div>
+    <button type="submit" value="Book" class="btn btn-primary col-w-2 col-s-2 center">Book</button>
+  <div class="col-w-1 col-s-1"></div>
+    <button type="button" class="btn btn-secondary col-w-2 col-s-2 center">cancel</button>
+     <div class="col-w-1 col-s-1"></div>
 </div>
-<div class="col-w-1"></div>
-<div class="col-w-3 center">
-Email:
-<input type="text" name="email" value="email">
-</div>
-</div>
-<br><br>
-<input type="submit" value="Book">
-
 <script type="text/javascript" href="/js/bootstrap.bundle.js" ></script>
 @endsection
