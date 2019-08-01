ca003bee1ea0f1415a5e0eab30e551827c1bec03
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-08-01 09:25:40 +0700

    adding id_Booking(unique)

--- database/migrations/2019_07_31_020017_create_rooms_table.php
+++ database/migrations/2019_07_31_020017_create_rooms_table.php
@@ -30,6 +30,7 @@ class CreateRoomsTable extends Migration
         });
         Schema::create('booklists', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('id_Booking')->unique;
             $table->unsignedBigInteger('id_Ruangan');
             $table->string('nama');
             $table->string('NPK');
