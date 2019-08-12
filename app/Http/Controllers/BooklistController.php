fbabbcfe18c78041566797d45254442c63ba6c5c
Jeremia Ronaldo Manurung <jeremiarm5@gmail.com>
2019-08-12 14:01:56 +0700

    left join 2x

--- app/Http/Controllers/BooklistController.php
+++ app/Http/Controllers/BooklistController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\booklist;
+use App\room;
 class BooklistController extends Controller
 {
     //
 	public function view(){
-        $list = booklist::all()->sortBy('id');  
+        $list = booklist::leftJoin('rooms','booklists.id_Ruangan','=','rooms.id')
+                        ->leftJoin('buildings','rooms.id_gedung','=','buildings.id')
+                        ->select('booklists.*','rooms.id as room_id','buildings.id as building_id')
+                        ->get();  
         return view('',compact('list'));
     }
 
