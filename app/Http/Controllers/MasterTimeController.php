8c36ab48774dbbbb14309478419c56b156d4173f
FarrasR <farrasrahmatullah@gmail.com>
2019-08-14 16:23:09 +0700

    benerin time jadi config

--- app/Http/Controllers/MasterTimeController.php
+++ app/Http/Controllers/MasterTimeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\mastertime;
 use Auth;
 class MasterTimeController extends Controller
 {
@@ -11,13 +10,10 @@ class MasterTimeController extends Controller
 
     public function update(Request $request){
         if(Auth::check()){
-          $input = mastertime::where('id',1)->first();
           $data = $this->validate($request, [
               'time'=>'required',
               ]);
-          $input->masterMinute=$data['time'];
-          $input->save();
-
+          Config::set(['booklists_timeout' => $data['time'] ]);
           return redirect('/admin/home');  
         }
         return redirect('/');
