b2834cddc8316ec862be22ce7de0c72a0fd9a637
FarrasR <farrasrahmatullah@gmail.com>
2019-08-16 11:04:33 +0700

    readme + sql dump

---
+++ MeetingRoom.sql
@@ -0,0 +1,123 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.26-MariaDB : Database - kpmr
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kpmr` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `kpmr`;
+
+/*Table structure for table `admins` */
+
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admins_username_unique` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `admins` */
+
+insert  into `admins`(`id`,`username`,`password`,`status`) values (1,'admin','$2y$10$vmllY/YjiiuoOiCFAlNVUeWca8/sqi0dwlLCESaTLdqWUeIdCWUSm','admin');
+
+/*Table structure for table `booklists` */
+
+DROP TABLE IF EXISTS `booklists`;
+
+CREATE TABLE `booklists` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `id_Ruangan` bigint(20) unsigned NOT NULL,
+  `nama` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `NPK` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PIN` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `waktu_Pinjam_Mulai` datetime NOT NULL,
+  `waktu_Pinjam_Selesai` datetime NOT NULL,
+  `waktu_Pinjam_Timeout` datetime NOT NULL,
+  `keperluan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `booklists_id_ruangan_foreign` (`id_Ruangan`),
+  CONSTRAINT `booklists_id_ruangan_foreign` FOREIGN KEY (`id_Ruangan`) REFERENCES `rooms` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `booklists` */
+
+/*Table structure for table `buildings` */
+
+DROP TABLE IF EXISTS `buildings`;
+
+CREATE TABLE `buildings` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_gedung` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_lantai` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `buildings` */
+
+/*Table structure for table `configs` */
+
+DROP TABLE IF EXISTS `configs`;
+
+CREATE TABLE `configs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `configs` */
+
+insert  into `configs`(`id`,`key`,`value`) values (1,'booklists_timeout','10');
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2019_07_31_020017_create_rooms_table',1);
+
+/*Table structure for table `rooms` */
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_ruangan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_gedung` bigint(20) unsigned NOT NULL,
+  `lantai` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status_now` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rooms_id_gedung_foreign` (`id_gedung`),
+  CONSTRAINT `rooms_id_gedung_foreign` FOREIGN KEY (`id_gedung`) REFERENCES `buildings` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `rooms` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,44 @@
-kdjfkdjf
-testing
+INSTALASI WAITING ROOM
+
+
+1. di command line execute "php artisan key:generate"
+
+2. di cronjob tambahkan "* * * * * cd {/path to your project} && php artisan schedule:run >> /dev/null 2>&1"
+	tanpa tanda petik dan ganti {/path to your project} dengan path project
+
+3. Untuk setting email di file .env 
+
+contoh di .env:
+
+MAIL_DRIVER=smtp
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USERNAME=exampleemail@gmail.com
+MAIL_PASSWORD="password" (jangan lupa petik untuk password)
+MAIL_ENCRYPTION=tls
+
+4. Untuk setting DB di file .env
+
+contoh di .env
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=dbname
+DB_USERNAME=dbusername
+DB_PASSWORD=dbpassword
+
+5. execute MeetingRoom.sql di mysql DB
+
+6. untuk setting template email ada di file resources/views/email.blade.php
+
+7. akses admin page di /admin dengan login 
+
+username: admin
+password: adminXmeetingYroomZ2019
+
+8. ganti password bisa di db dengan hash bcrypt di https://bcrypt-generator.com/
+
+9. di command line execute "php artisan serve" tanpa tanda petik
+
+10. bila ada pertanyaan mohon email jeremiarm5@gmail.com atau WA 085749422933  
